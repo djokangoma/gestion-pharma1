3ba0a4e639f672d04fbb1fc6d655b47967304808
djo kangoma <djokangoma@gmail.com>
2023-12-04 14:11:14 +0100

    add login page

---
+++ src/pages/about.jsx
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <>
+      <h1>A propos de nous</h1>
+      <p>ceci est une application de gestion des produits pharmaceutiques </p>
+    </>
+  );
+}
+export default About;
