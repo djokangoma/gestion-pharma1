05fb550cd72d214536a4fe518c6c361b4cdf5a7d
djo kangoma <djokangoma@gmail.com>
2023-12-04 12:44:04 +0100

    add layout component

--- src/components/dashboard-navbar/dashboard-navbar.jsx
+++ src/components/dashboard-navbar/dashboard-navbar.jsx
@@ -1,4 +1,22 @@
 function DashboardNavbar() {
-  return;
-  <ul></ul>;
+  return (
+    <ul>
+      <li>
+        <NavLink to="/all-products">All-products</NavLink>
+      </li>
+      <li>
+        <NavLink to="/create-products">Create-products</NavLink>
+      </li>
+      <li>
+        <NavLink to="/update-product">Update-product</NavLink>
+      </li>
+      <li>
+        <NavLink to="/delete-product">Delete-product</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Deconnexion</NavLink>
+      </li>
+    </ul>
+  );
 }
+export default DashboardNavbar;
---
+++ src/components/layout/layout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+function Layout() {
+  return (
+    <>
+      <nav>
+        <Navbar />
+      </nav>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
+export default Layout;
